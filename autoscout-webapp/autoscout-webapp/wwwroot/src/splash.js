33aa8e4528af69d84d99eb6d7c7b2dd8c379f860
swizkon <jonas@jerndin.se>
2017-12-20 00:43:42 +0100

    Added vuetify

--- autoscout-webapp/autoscout-webapp/wwwroot/src/splash.js
+++ autoscout-webapp/autoscout-webapp/wwwroot/src/splash.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Splash from './components/Splash'
+import Vuetify from 'vuetify'
+ 
 
 import "vueify/lib/insert-css"
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
