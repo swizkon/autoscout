f09864a301dc6387c3f5b3cdb84693df87d4b4f9
swizkon <jonas@jerndin.se>
2017-12-20 00:31:35 +0100

    Start of the new forms.

--- autoscout-webapp/autoscout-webapp/wwwroot/src/components/Hello.vue
+++ autoscout-webapp/autoscout-webapp/wwwroot/src/components/Hello.vue
@@ -2,12 +2,12 @@
   <div class="hello">
     <h1>{{ label }}</h1>
     <form v-on:submit.prevent="onSubmit">
-      <input class="input-lg" name="entityid" type="number" v-model="entityId" placeholder="Enter entity id" />
+      <input class="input-lg" name="entityid" v-model="entityId" placeholder="Enter entity id" />
     </form>
     - or select preview mock -
-    <ul v-if="mocks">
-      <li v-for="d in mocks">
-        <a :href="'/accounts.html#/mocks/' + d">{{ d }}</a>
+    <ul v-if="items">
+      <li v-for="item in items">
+        <a :href="'/accounts.html#/mocks/' + item">{{ item }}</a>
       </li>
     </ul>
   </div>
@@ -18,24 +18,31 @@
     name: 'hello',
     data() { 
       return {
-        label: `Enter entity id`,
+        label: `Enter item`,
         entityId: '12345',
-        mocks: null
+        items: null
       }
     },
     created () {
         var _this = this;
+            _this.items = [];
+          /*
           this.$toasted.info('Loading pre-defined template')
           fetch('/api/accountEvent/mocks').then(function(response) {
             return response.json();
           }).then(function(jsonData) {
-            _this.mocks = jsonData
+            _this.items = []
           });
+          */
     },
     methods: {
         onSubmit: function (event) {
-          this.$toasted.info('Go to history for entity ' + this.entityId)
-          this.$router.push({ name: 'AccountHistory', params: { id: this.entityId}})
+          this.$toasted.info('Add item ' + this.entityId)
+          
+          // this.$router.push({ name: 'AccountHistory', params: { id: this.entityId}})
+        this.items.push(this.entityId)
+
+          this.entityId = "";
         }
       }
   }
--- front/js/app/AutoScout.App.js
+++ front/js/app/AutoScout.App.js
@@ -5,20 +5,11 @@ jQuery.fn.toObservable = function (eventName, selector) {
 	return Rx.Observable.fromEvent(this, eventName, selector);
 };
 
-
 var api = (function () {
 
 	var baseUrl = "../data";
 
 	baseUrl = "http://localhost:3004";
-
-	/*
-
-	
-			var promise = $.ajax({
-				url: 'https://yaq8h11crk.execute-api.eu-west-1.amazonaws.com/prod/pads?owner=' + encodeURI(inp.val())
-			}).promise();
-			 */
 
 	return {
 		"pads": {
@@ -52,12 +43,8 @@ var api = (function () {
 // Flow and such
 
 var event$ = new Rx.Subject();
-event$.subscribe(console.log);
 
 var createdPad$ = event$.filter(f => f["PadCreated"]).map(d => d["PadCreated"]);
-
-createdPad$.subscribe(f => console.log(f));
-
 
 event$.onNext({
 	"PadCreated": {
@@ -89,9 +76,7 @@ var loginView = new Vue({
 });
 
 
-// var usernameChange$ = Rx.Observable.fromEvent($('#app-login-view form input'), 'change');
 var usernameChange$ = $('#app-login-view form input').toObservable('keyup');
-//  Rx.Observable.fromEvent($('#app-login-view form input'), 'keyup');
 
 usernameChange$.delay(300)
 			   .map(function(evt){
@@ -120,7 +105,6 @@ var login$ = Rx.Observable.create(function (observer) {
 			loginView.loginRequired = false;
 
 			observer.onNext(data);
-			// observer.onNext(JSON.parse(data));
 		});
 
 		return false;
@@ -140,22 +124,7 @@ var createdPad$ = Rx.Observable.create(function (observer) {
 	$('#app-main-view form').on('submit', function () {
 		var f = this;
 		var inp = $(f).find('input');
-
 		alert('Form');
-		/*event$.onNext({
-			"PadCreated": {
-				"title": inp.val()
-			}
-			});
-			*/
-		// padname, owner
-		/*
-		var doley = Rx.Observable.fromPromise(api.pads.createPad(inp.val()));
-
-        doley.subscribe(function(data){
-			observer.onNext(JSON.parse(data));
-        });
-		*/
 		return false;
 	});
 });
@@ -170,8 +139,6 @@ var mainView = new Vue({
 		pads: []
 	}
 });
-
-
 
 var booksView = new Vue({
 	el: '#app-book-view',
@@ -212,7 +179,6 @@ var removedBook$ = new Rx.Subject();
 var book$ = Rx.Observable.merge(addedBook$, removedBook$);
 
 tick$.subscribe(function (tick) {
-	// console.log(tick);
 	var id = tick.toString();
 	var book = { "id": id, "title": "Book " + id };
 	addedBook$.onNext(book);
@@ -223,50 +189,8 @@ book$.subscribe(function (book) {
 	booksView.books.push(book);
 });
 
-
 var tick2$ = Rx.Observable.interval(2000)
 	.map(function (t) { return new Date().getTime() });
 
-
 var clickStream = Rx.Observable.fromEvent($(document), 'bookSelected');
 clickStream.subscribe(console.log);
-/*
-
-
-	var button, clickStream;
-
-		button = document.querySelector('button');
-		// console.log(button);
-		// alert(button);
-		clickStream = Rx.Observable.fromEvent(button, 'click');
-
-
-	// HERE
-	// The 4 lines of code that make the multi-click logic
-	var multiClickStream = clickStream
-	    .buffer(function() { return clickStream.throttle(250); })
-	    .map(function(list) { return list.length; })
-	    .filter(function(x) { return x >= 2; });
-
-	// Same as above, but detects single clicks
-	var singleClickStream = clickStream
-	    .buffer(function() { return clickStream.throttle(300); })
-	    .map(function(list) { return list.length; })
-	    .filter(function(x) { return x === 1; });
-
-	// Listen to both streams and render the text label accordingly
-	singleClickStream.subscribe(function (event) {
-	    document.querySelector('h2.singles').textContent = 'SIngle click';
-	});
-
-	multiClickStream.subscribe(function (numclicks) {
-	    document.querySelector('h2.multies').textContent = ''+numclicks+'x click';
-	});
-	Rx.Observable.merge(singleClickStream, multiClickStream)
-	    .throttle(1000)
-	    .subscribe(function (suggestion) {
-	        document.querySelector('h2').textContent = '';
-	    });
-
-
- */
